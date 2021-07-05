f5e60b554f6939877c3de91614a86241ff54534d
Ghazal <ghazal.kazemi@gmail.com>
2021-07-05 17:01:31 +1000

    Fix typo in the spring link

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,7 +8,7 @@ import {map} from "rxjs/operators";
   providedIn: 'root'
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/produsts';
+  private baseUrl = 'http://localhost:8080/api/products';
   constructor(private httpClient:HttpClient) { }
 
   getProductList(): Observable<Product[]>{
