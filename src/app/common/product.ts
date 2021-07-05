c5ddbc458c65e0e5767eb1588d9bca1b92e075f8
Ghazal <ghazal.kazemi@gmail.com>
2021-07-05 17:28:48 +1000

    Fix the typo in unitsInStock

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -5,18 +5,18 @@ export class Product {
   unitPrice: number;
   imageUrl: string;
   active: boolean;
-  unitInStock: number;
+  unitsInStock: number;
   dateCreated: Date;
   lastUpdated: Date;
 
-  constructor(sku: string, name: string, description: string, unitPrice: number, imageUrl: string, active: boolean, unitInStock: number, dateCreated: Date, lastUpdated: Date) {
+  constructor(sku: string, name: string, description: string, unitPrice: number, imageUrl: string, active: boolean, unitsInStock: number, dateCreated: Date, lastUpdated: Date) {
     this.sku = sku;
     this.name = name;
     this.description = description;
     this.unitPrice = unitPrice;
     this.imageUrl = imageUrl;
     this.active = active;
-    this.unitInStock = unitInStock;
+    this.unitsInStock = unitsInStock;
     this.dateCreated = dateCreated;
     this.lastUpdated = lastUpdated;
   }
