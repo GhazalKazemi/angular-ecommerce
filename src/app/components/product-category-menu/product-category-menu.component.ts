248c0aef297754c06e741699796e92189ae6b23a
Ghazal <ghazal.kazemi@gmail.com>
2021-07-07 14:03:35 +1000

    create produc-category component

--- src/app/components/product-category-menu/product-category-menu.component.ts
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductCategory} from "../../common/product-category";
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-product-category-menu',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-category-menu.component.css']
 })
 export class ProductCategoryMenuComponent implements OnInit {
+  productCategories: ProductCategory[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
+    this.productCategories = [];
   }
 
+  ngOnInit(): void {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+    this.productService.getProductCategries().subscribe(
+      data => {
+        console.log('Product Categories: ' + JSON.stringify(data));
+        this.productCategories = data
+      });
+  }
 }
