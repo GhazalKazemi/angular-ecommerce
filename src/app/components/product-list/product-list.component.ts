4684d178d31fc06e783815ea7634ebe82d76dc7e
Ghazal <ghazal.kazemi@gmail.com>
2021-07-07 17:20:16 +1000

    improve product-list component to handle serached products

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,9 +12,11 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
   constructor(private productService: ProductService, private route: ActivatedRoute) {
     this.products = [];
     this.currentCategoryId = 0;
+    this.searchMode = false;
   }
 
   ngOnInit(): void {
@@ -24,6 +26,24 @@ export class ProductListComponent implements OnInit {
 
   }
   listProducts(){
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode){
+      this.handleSearchProducts();
+    }else {
+      this.handleListProducts();
+    }
+  }
+  handleSearchProducts(){
+    // @ts-ignore
+    const keyword: string = this.route.snapshot.paramMap.get('keyword');
+    this.productService.searchProducts(keyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+
+  }
+  handleListProducts(){
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId){
 
@@ -39,5 +59,4 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
-
 }
