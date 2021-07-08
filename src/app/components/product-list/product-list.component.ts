e967e45969b13beb9f75674ad426216390c23e7f
Ghazal <ghazal.kazemi@gmail.com>
2021-07-08 17:18:47 +1000

    Change page size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -16,7 +16,7 @@ export class ProductListComponent implements OnInit {
   searchMode: boolean = false;
 
   pageNumber: number = 1;
-  pageSize: number = 10;
+  pageSize: number = 5;
   totalElements: number =0;
 
   constructor(private productService: ProductService, private route: ActivatedRoute) {
